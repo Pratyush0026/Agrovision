72704c8b69ffb14dfcfba1f4e316e80ed14a1ebd
Pratyush0026 <113766134+Pratyush0026@users.noreply.github.com>
2023-06-25 12:10:41 +0530

    pushed

--- BuyerPortal/BuyerHomepage.php
+++ BuyerPortal/BuyerHomepage.php
@@ -10,7 +10,7 @@
 	<meta http-equiv="Content-Type" content="text/html; charset=windows-1252">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-	<title>Agrocraft Homepage</title>
+	<title>AgroVision Homepage</title>
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 	<script src="https://kit.fontawesome.com/c587fc1763.js" crossorigin="anonymous"></script>
 
